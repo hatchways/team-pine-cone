1a089d26cd8205a8630cb103f267c72164e07547
Dan Proctor <dproc96@gmail.com>
2020-10-14 19:05:48 -0400

    Update ProfileSettings.js
    Added basic styling and icons

--- client/src/pages/ProfileSettings.js
+++ client/src/pages/ProfileSettings.js
@@ -1,6 +1,16 @@
-import { Drawer, List, ListItem } from '@material-ui/core';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, withStyles } from '@material-ui/core';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { AccountCircle, Photo, CreditCard, Security, Settings } from '@material-ui/icons'
+
+const styles = theme => ({
+  linkText: {
+    textDecoration: "none",
+    color: "#222222",
+    fontWeight: 700,
+    marginRight: 5,
+  }
+});
 
 class ProfileSettings extends Component {
     constructor(props) {
@@ -13,40 +23,49 @@ class ProfileSettings extends Component {
           {
             title: "Edit Profile",
             path: `${origin}/edit-profile`,
+            icon: <AccountCircle />
           },
           {
             title: "Profile Photo",
             path: `${origin}/profile-photo`,
+            icon: <Photo />
           },
           {
             title: "Payment",
             path: `${origin}/payment`,
+            icon: <CreditCard />
           },
           {
             title: "Security",
             path: `${origin}/security`,
+            icon: <Security />
           },
           {
             title: "Settings",
             path: `${origin}/settings`,
+            icon: <Settings />
           },
         ];
     }
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                <Drawer open={this.state.isSidebarOpen}>
-                    <List>
-                        {this.links.map(link => (
-                            <Link to={link.path}>
-                                <ListItem>{link.title}</ListItem>
-                            </Link>
-                        ))}
-                    </List>
-                </Drawer>
-            </div>
+          <div>
+            <Drawer open={this.state.isSidebarOpen}>
+              <List>
+                {this.links.map((link) => (
+                  <Link className={classes.linkText} to={link.path}>
+                    <ListItem>
+                        <ListItemIcon>{link.icon}</ListItemIcon>
+                      <ListItemText>{link.title}</ListItemText>
+                    </ListItem>
+                  </Link>
+                ))}
+              </List>
+            </Drawer>
+          </div>
         );
     }
 }
 
-export default ProfileSettings;
+export default withStyles(styles)(ProfileSettings);

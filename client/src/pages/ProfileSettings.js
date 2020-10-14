6cbfb008f7fd12bf9d01da2f79aef42363226ff8
Dan Proctor <dproc96@gmail.com>
2020-10-14 19:52:27 -0400

    Update ProfileSettings.js
    Added mobile responsiveness

--- client/src/pages/ProfileSettings.js
+++ client/src/pages/ProfileSettings.js
@@ -1,7 +1,23 @@
-import { Card, Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar, withStyles } from '@material-ui/core';
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { AccountCircle, Photo, CreditCard, Security, Settings } from '@material-ui/icons'
+import {
+  Card,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  withStyles,
+  withWidth,
+} from "@material-ui/core";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  AccountCircle,
+  Photo,
+  CreditCard,
+  Security,
+  Settings,
+} from "@material-ui/icons";
 
 const drawerWidth = 240;
 const styles = (theme) => ({
@@ -13,9 +29,15 @@ const styles = (theme) => ({
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
+    [theme.breakpoints.between("xs", "sm")]: {
+      width: 50,
+    },
   },
   drawerPaper: {
     width: drawerWidth,
+    [theme.breakpoints.between("xs", "sm")]: {
+      width: 50,
+    },
   },
   drawerContainer: {
     overflow: "auto",
@@ -26,71 +48,75 @@ const styles = (theme) => ({
   },
   root: {
     display: "flex",
-    backgroundColor: '#fbfbfb',
-    height: '100vh'
+    backgroundColor: "#fbfbfb",
+    height: "100vh",
   },
   card: {
-      padding: theme.spacing(3)
-  }
+    padding: theme.spacing(3),
+  },
 });
 
 class ProfileSettings extends Component {
-    constructor(props) {
-        super(props)
-        const origin = '/me'
-        this.links = [
-          {
-            title: "Edit Profile",
-            path: `${origin}/edit-profile`,
-            icon: <AccountCircle />
-          },
-          {
-            title: "Profile Photo",
-            path: `${origin}/profile-photo`,
-            icon: <Photo />
-          },
-          {
-            title: "Payment",
-            path: `${origin}/payment`,
-            icon: <CreditCard />
-          },
-          {
-            title: "Security",
-            path: `${origin}/security`,
-            icon: <Security />
-          },
-          {
-            title: "Settings",
-            path: `${origin}/settings`,
-            icon: <Settings />
-          },
-        ];
-    }
-    render() {
-        const { classes } = this.props;
-        return (
-          <div className={classes.root}>
-            <Drawer variant="permanent" className={classes.drawer} classes={{paper: classes.drawerPaper}}>
-                <Toolbar />
-              <List className={classes.drawerContainer}>
-                {this.links.map((link) => (
-                  <Link className={classes.linkText} to={link.path}>
-                    <ListItem>
-                        <ListItemIcon>{link.icon}</ListItemIcon>
-                      <ListItemText>{link.title}</ListItemText>
-                    </ListItem>
-                  </Link>
-                ))}
-              </List>
-            </Drawer>
-            <div className={classes.content}>
-                <Card className={classes.card}>
-                    Content
-                </Card>
-            </div>
-          </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    const origin = "/me";
+    this.links = [
+      {
+        title: "Edit Profile",
+        path: `${origin}/edit-profile`,
+        icon: <AccountCircle />,
+      },
+      {
+        title: "Profile Photo",
+        path: `${origin}/profile-photo`,
+        icon: <Photo />,
+      },
+      {
+        title: "Payment",
+        path: `${origin}/payment`,
+        icon: <CreditCard />,
+      },
+      {
+        title: "Security",
+        path: `${origin}/security`,
+        icon: <Security />,
+      },
+      {
+        title: "Settings",
+        path: `${origin}/settings`,
+        icon: <Settings />,
+      },
+    ];
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <Drawer
+          variant="permanent"
+          className={classes.drawer}
+          classes={{ paper: classes.drawerPaper }}
+        >
+          <Toolbar />
+          <List className={classes.drawerContainer}>
+            {this.links.map((link) => (
+              <Link className={classes.linkText} to={link.path}>
+                <ListItem>
+                  <ListItemIcon>{link.icon}</ListItemIcon>
+                  {this.props.width !== "xs" && this.props.width !== "sm" && (
+                    <ListItemText>{link.title}</ListItemText>
+                  )}
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </Drawer>
+        <div className={classes.content}>
+          <Card className={classes.card}>Content</Card>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default withStyles(styles)(ProfileSettings);
+export default withStyles(styles)(withWidth()(ProfileSettings));

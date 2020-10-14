4d0fa10e55f517aa89807096e713df789345b039
Dan Proctor <dproc96@gmail.com>
2020-10-14 18:07:16 -0400

    Update Database.spec.js
    Added first test cases for successful doc save

--- server/test/Database.spec.js
+++ server/test/Database.spec.js
@@ -1,7 +1,8 @@
 const chai = require('chai');
 const mongoose = require('mongoose');
+const db = require('../models');
 
-chai.should()
+chai.should();
 
 describe('Database Tests', () => {
     before(done => {
@@ -10,5 +11,31 @@ describe('Database Tests', () => {
         db.once("open", function () {
           done();
         });
+    })
+    describe('Profile Model', () => {
+      it('Should save with all fields filled correctly', done => {
+        const testProfile = new db.Profile({
+          firstName: 'John',
+          lastName: 'Smith',
+          gender: 'male',
+          birthDate: new Date('01-01-1990'),
+          description: 'This is not a test',
+          availability: [
+            {
+              start: new Date('10-01-2020'),
+              end: new Date('10-31-2020')
+            }
+          ]
+        })
+        testProfile.save(done)
+      });
+
+      it ('Should save with only first and last name filled', done => {
+        const testProfile = new db.Profile({
+          firstName: 'John',
+          lastName: 'Smith'
+        })
+        testProfile.save(done)
+      });
     })
 })

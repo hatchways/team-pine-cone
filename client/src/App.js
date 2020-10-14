a323046510003b397c41a83ab234404ef9191972
Michael <singh.michaelanthony@gmail.com>
2020-10-13 21:15:16 -0400

    create signup page

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 
+
+import CssBaseline from '@material-ui/core/CssBaseline'; // CSS Reset
 import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+// import LandingPage from "./pages/Landing";
+import SignUp from "./pages/SignUp"
 
 import "./App.css";
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
+      <CssBaseline />
+        <BrowserRouter>
+          <Route path="/" component={SignUp} />
+        </BrowserRouter>
     </MuiThemeProvider>
   );
 }

f59a150eb05899c94dc1a52496450e1e3561ad94
Dan Proctor <dproc96@gmail.com>
2020-10-15 19:05:17 -0400

    Update useForm.js
    Added Checkbox specific on change due to error

--- client/src/components/useForm.js
+++ client/src/components/useForm.js
@@ -6,14 +6,19 @@ export default function useForm(initialFormValues) {
   const [errors, setErrors] = useState(initialFormValues);
 
   const handleInputChange = e => {
-    let { name, value, checked } = e.target;
-    if (!value) {
-      value = checked
-    }
+    let { name, value } = e.target;
     setValues({
       ...values,
       [name]: value
     })
+  };
+
+  const handleCheckboxChange = (e) => {
+    let { name, checked } = e.target;
+    setValues({
+      ...values,
+      [name]: checked,
+    });
   };
 
   // Return object of re-usable variables or functions
@@ -22,6 +27,7 @@ export default function useForm(initialFormValues) {
     setValues,
     errors,
     setErrors,
-    handleInputChange
+    handleInputChange,
+    handleCheckboxChange
   }
 }

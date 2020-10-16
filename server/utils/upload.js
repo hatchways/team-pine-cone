a3176073b8b1ae18bfb3d80fc8f525f6cd00b4d3
Dan Proctor <dproc96@gmail.com>
2020-10-16 19:43:42 -0400

    Update upload.js
    Added function to upload file to S3

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -6,3 +6,16 @@ AWS.config.update({
 });
 
 const s3 = new AWS.S3();
+
+const uploadFile = (buffer, name, type) => {
+    const params = {
+        ACL: "public-read",
+        Body: buffer,
+        Bucket: process.env.S3_BUCKET,
+        ContentType: type.mime,
+        Key: `${name}.${type.ext}`
+    }
+    return s3.upload(params).promise()
+}
+
+module.exports = uploadFile

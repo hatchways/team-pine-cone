151eeea392a839586b963eb7bc26a1e08d8b42bd
Dan Proctor <dproc96@gmail.com>
2020-10-17 13:58:17 -0400

    Update upload.js
    Added file parsing

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const multiparty = require("multiparty");
+const fs = require("fs")
+const fileType = require("file-type")
 const router = express.Router();
 
 router.post("/", (req, res) =>{
-    console.log(req.body)
+    const form = new multiparty.Form()
+    form.parse(req, async (err, fields, files) => {
+      const buffer = fs.readFileSync(files.file[0].path)
+      const type = await fileType.fromBuffer(buffer)
+      const name = files.file[0].originalFilename.replace(`.${type.ext}`, "");
+    });
 })
 
 module.exports= router;

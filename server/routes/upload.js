50c1d0f920f74e8626e7e13ccf331c18375fa58c
Dan Proctor <dproc96@gmail.com>
2020-10-17 14:31:22 -0400

    Update upload.js

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -2,15 +2,28 @@ const express = require("express");
 const multiparty = require("multiparty");
 const fs = require("fs")
 const fileType = require("file-type")
+
+const upload = require("../utils/upload")
+
 const router = express.Router();
 
 router.post("/", (req, res) =>{
-    const form = new multiparty.Form()
-    form.parse(req, async (err, fields, files) => {
-      const buffer = fs.readFileSync(files.file[0].path)
-      const type = await fileType.fromBuffer(buffer)
-      const name = files.file[0].originalFilename.replace(`.${type.ext}`, "");
-    });
+    try {
+        const form = new multiparty.Form()
+        form.parse(req, async (err, fields, files) => {
+          const buffer = fs.readFileSync(files.file[0].path)
+          const type = await fileType.fromBuffer(buffer)
+          const name = files.file[0].originalFilename.replace(`.${type.ext}`, "");
+          upload(buffer, name, type).then(result => {
+              // Add image to the user profile
+              res.json({url: result.Location})
+          })
+        });
+    }
+    catch (e) {
+        console.log(e)
+        res.end()
+    }
 })
 
 module.exports= router;

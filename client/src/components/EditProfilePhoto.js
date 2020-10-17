694a030a349d81ca84a5faf52f7b76f8fbd3aa58
Dan Proctor <dproc96@gmail.com>
2020-10-17 11:19:55 -0400

    Update EditProfilePhoto.js
    Added logic for handling file upload

--- client/src/components/EditProfilePhoto.js
+++ client/src/components/EditProfilePhoto.js
@@ -1,8 +1,8 @@
-import { Avatar, Button, Link, makeStyles } from '@material-ui/core';
+import { Avatar, Button, Input, Link, makeStyles } from '@material-ui/core';
 import { Delete } from '@material-ui/icons';
-import React from 'react';
+import React, { useState } from 'react';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     root: {
         display: "flex",
         flexDirection: "column",
@@ -19,18 +19,46 @@ const useStyles = makeStyles({
         fontSize: 12,
         fontStyle: "italic"
     }
-})
+}))
 
 function EditProfilePhoto(props) {
     const classes = useStyles()
+    const [image, setImage] = useState(null);
+    const handleChange = e => {
+        const file = Array.from(e.target.files)[0]
+        const formData = new FormData()
+        formData.append(0, file)
+        // POST formData to backend when integrating
+
+        // setImage to url
+    }
     return (
-        <div className={classes.root}>
-            <h2>Profile Photo</h2>
-            <Avatar className={classes.photo}>D</Avatar>
-            <p className={classes.helper}>Please select a photo that clearly shows your face</p>
-            <Button variant="outlined">Upload a file from your device</Button>
-            <Button variant="outlined"><Delete style={{marginRight: 5}} /> Delete Photo</Button>
-        </div>
+      <div className={classes.root}>
+        <h2>Profile Photo</h2>
+        {image ? (
+          <Avatar className={classes.photo} alt="profile" src={image} />
+        ) : (
+          <Avatar className={classes.photo}>No Photo</Avatar>
+        )}
+        <p className={classes.helper}>
+          Please select a photo that clearly shows your face
+        </p>
+        <input
+          onChange={handleChange}
+          accept="image/*"
+          style={{ display: "none" }}
+          id="photo-upload"
+          type="file"
+        />
+        <label htmlFor="photo-upload">
+          <Button size="large" color="primary" variant="outlined" component="span">
+            Upload a file from your device
+          </Button>
+        </label>
+        <Button size="small" variant="outlined">
+          <Delete fontSize="small" style={{ marginRight: 5 }} /> Delete Photo
+        </Button>
+      </div>
     );
 }
 

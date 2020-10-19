aa8fe03cf669f165d59b0a74dd85a8b9495585ba
Dan Proctor <dproc96@gmail.com>
2020-10-19 18:52:50 -0400

    Update Request.js
    Built schema

---
+++ server/models/Request.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+const requestSchema = new Schema({
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Profile",
+    required: true,
+  },
+  sitter_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Profile",
+    required: true,
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+    required: true,
+  },
+  accepted: {
+    type: Boolean,
+    default: false,
+  },
+  declined: {
+    type: Boolean,
+    default: false,
+  },
+  paid: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+
+
+const Request = model("Request", requestSchema);
+
+module.exports = Request;

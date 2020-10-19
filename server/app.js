f45b362a1ba9c7ada42a1c9126f025c0736b38f0
Michael Braga <michael_braga@outlook.com>
2020-10-19 12:36:59 -0400

    Added POST /logout route, created logout button at ProfileSettings

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
 const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
+const logoutRouter = require('./routes/logout');
 const userRouter = require('./routes/user');
 
 const { json, urlencoded } = express;
@@ -39,6 +40,7 @@ app.use("/ping", pingRouter);
 app.use('/user', userRouter);
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
+app.use("/logout", logoutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

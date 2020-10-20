2770486e5ee71f6204756fc15a07189836357c0c
Dan Proctor <dproc96@gmail.com>
2020-10-20 19:44:09 -0400

    Update profile.js
    Added route for get my profile

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -7,8 +7,10 @@ const {
   createProfile, 
   updateProfile, 
   getProfiles,
-  getProfile
+  getProfile,
+  getMyProfile
 } = require("../controllers/profile");
+const { authenticate } = require("../middleware/authenticate");
 
 const Router = express.Router();
 
@@ -16,5 +18,6 @@ Router.post("/create", profileCreateValidator, createProfile);
 Router.put("/:id", profileUpdateValidator, updateProfile);
 Router.get("/", getProfiles);
 Router.get("/:id", getProfile);
+Router.get("/me", authenticate, getMyProfile);
 
 module.exports = Router;

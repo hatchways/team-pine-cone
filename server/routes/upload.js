c25c4c0bd5f3f6060ab6f7dd627da9c3e6554f34
Dan Proctor <dproc96@gmail.com>
2020-10-20 16:52:52 -0400

    Update upload.js
    Added delete route and add photo to Profile document on upload

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,13 +1,19 @@
+const createError = require("http-errors");
 const express = require("express");
 const multiparty = require("multiparty");
 const fs = require("fs")
 const fileType = require("file-type")
 
-const upload = require("../utils/upload")
+const upload = require("../utils/upload");
+const Profile = require("../models/Profile");
+const authenticate = require("../middleware/authenticate");
 
 const router = express.Router();
 
-router.post("/", (req, res) =>{
+router.post("/", authenticate, (req, res, next) =>{
+    if (!req.user) {
+      return next(createError(403));
+    }
     try {
         const form = new multiparty.Form()
         form.parse(req, async (err, fields, files) => {
@@ -15,15 +21,32 @@ router.post("/", (req, res) =>{
           const type = await fileType.fromBuffer(buffer)
           const name = files.file[0].originalFilename.replace(`.${type.ext}`, "") + "-" + new Date();
           upload(buffer, name, type).then(result => {
-              // Add image to the user profile
+              Profile.findById(req.user.profile).then(profile => {
+                profile.photo = result.Location;
+                profile.save()
+              })
               res.json({url: result.Location})
           })
         });
     }
     catch (e) {
         console.log(e)
-        res.end()
+        return next(createError(503))
     }
+})
+
+router.put("/delete", authenticate, (req, res, next) => {
+    if (!req.user) {
+      return next(createError(403));
+    }
+    Profile.findById(user.profile).then(profile => {
+        profile.photo = null;
+        profile.save();
+        res.status(200).end();
+    }).catch(e => {
+        console.log(e)
+        next(createError(503));
+    })
 })
 
 module.exports= router;

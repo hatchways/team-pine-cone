c0b2d67a8204ed046c4e75b5e5b6a25ca0fe0756
Dan Proctor <dproc96@gmail.com>
2020-10-20 19:21:30 -0400

    Update profile.js
    Added test photo for set up

--- client/src/contexts/profile.js
+++ client/src/contexts/profile.js
@@ -8,8 +8,11 @@ export const useProfileContext = () => {
 }
 
 export function ProfileProvider({ children }) {
-    const [profile, setProfile] = useState(null)
+    const [profile, setProfile] = useState({
+      photo:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+    });
     return (
-        <ProfileContext.Provider value={profile, setProfile}>{children}</ProfileContext.Provider>
+        <ProfileContext.Provider value={{profile, setProfile}}>{children}</ProfileContext.Provider>
     )
 }

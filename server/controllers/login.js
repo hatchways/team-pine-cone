db74ade8539dfb0ed45b282f6841b34ac6aa95c0
Dan Proctor <dproc96@gmail.com>
2020-10-19 20:16:40 -0400

    fixed linting and performed a lint

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,40 +1,40 @@
-const createError = require('http-errors');
-const { validationResult } = require('express-validator');
-const { User } = require('../models/');
-const { jwtCookie } = require('../utils/jwt/');
+const createError = require("http-errors");
+const { validationResult } = require("express-validator");
+const { User } = require("../models/");
+const { jwtCookie } = require("../utils/jwt/");
 
 const loginUser = async (req, res, next) => {
-	const { email, password } = req.body;
-	const errors = validationResult(req);
+  const { email, password } = req.body;
+  const errors = validationResult(req);
 
-	if (!errors.isEmpty()) {
-		return next(createError(422, 'Email and password are both required.'));
-	}
+  if (!errors.isEmpty()) {
+    return next(createError(422, "Email and password are both required."));
+  }
 	
-	try {
-		const user = await User.findOne({email: email.toLowerCase()});
+  try {
+    const user = await User.findOne({email: email.toLowerCase()});
 
-		if (!user) {
-			return next(createError(404, 'User not found.'));
-		}
+    if (!user) {
+      return next(createError(404, "User not found."));
+    }
 
-		const passwordsMatch = await user.verifyPassword(password);
+    const passwordsMatch = await user.verifyPassword(password);
 
-		if (!passwordsMatch) {
-			return next(createError(403));
-		}
+    if (!passwordsMatch) {
+      return next(createError(403));
+    }
 
-		jwtCookie(user, res);
+    jwtCookie(user, res);
 
-		return res
-			.status(200)
-			.json({ user: {
-				id: user.id,
-				email: user.email
-			}});
-	} catch (err) {
-		next(createError(500, err.message));
-	}
+    return res
+      .status(200)
+      .json({ user: {
+        id: user.id,
+        email: user.email
+      }});
+  } catch (err) {
+    next(createError(500, err.message));
+  }
 };
 
 module.exports = { loginUser };

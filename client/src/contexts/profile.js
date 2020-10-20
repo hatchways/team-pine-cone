21395f8a161acd169915d96dbcf73ef901f22428
Dan Proctor <dproc96@gmail.com>
2020-10-20 19:44:19 -0400

    Update profile.js
    Added logic for fetching profile

--- client/src/contexts/profile.js
+++ client/src/contexts/profile.js
@@ -1,4 +1,4 @@
-import React, {useContext, createContext, useState} from 'react';
+import React, {useContext, createContext, useState, useEffect} from 'react';
 
 const ProfileContext = createContext(null);
 
@@ -8,10 +8,12 @@ export const useProfileContext = () => {
 }
 
 export function ProfileProvider({ children }) {
-    const [profile, setProfile] = useState({
-      photo:
-        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
-    });
+    const [profile, setProfile] = useState(null);
+    useEffect(() => {
+        fetch("/profile/me").then(profile => {
+            setProfile(profile)
+        })
+    }, [])
     return (
         <ProfileContext.Provider value={{profile, setProfile}}>{children}</ProfileContext.Provider>
     )

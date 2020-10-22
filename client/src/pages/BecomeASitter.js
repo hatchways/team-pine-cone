86609576e1f6e1fa59f8849c6cf0f7d633f54ec4
Dan Proctor <dproc96@gmail.com>
2020-10-21 21:04:02 -0400

    Update BecomeASitter.js
    Become A Sitter updates you to being a sitter and redirects you to availability

--- client/src/pages/BecomeASitter.js
+++ client/src/pages/BecomeASitter.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import { Card, CircularProgress, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import { useProfileContext } from '../contexts/profile';
 
 function BecomeASitter(props) {
+    const [redirect, setRedirect] = useState(false)
+    const { profile, setProfile } = useProfileContext()
+    useEffect(() => {
+        const profileUpdate = {
+            ...profile,
+            isSitter: true
+        }
+        const options = {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(profileUpdate)
+        }
+        fetch(`/profile/${profile._id}`, options).then(response => {
+            response.json().then(result => {
+                setProfile(result.profile)
+                setRedirect(true)
+            })
+        })
+    }, [setProfile, setRedirect, profile])
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <Toolbar />
+            {
+                redirect ?
+                <Redirect to="/me/availability" /> :
+                <CircularProgress style={{margin: "20px auto", display: "block"}} />
+            }
+        </Fragment>
     );
 }
 

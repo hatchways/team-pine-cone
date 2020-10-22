9ed3ff27a5eaadb81b044db26d29b522ddc8f483
Dan Proctor <dproc96@gmail.com>
2020-10-21 20:32:31 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import ProfileDetails from './pages/ProfileDetails';
 import { AuthProvider } from "./contexts/user";
 import PrivateRoute from "./components/PrivateRoute/";
 import { ProfileProvider } from "./contexts/profile";
+import BecomeASitter from "./pages/BecomeASitter";
 
 function App() {
   return (
@@ -26,7 +27,7 @@ function App() {
             <Route exact path="/"/>
             <PrivateRoute path="/me" component={ProfileSettings} />
             <Route path="/profiles/:id" component={ProfileDetails} />
-            <PrivateRoute exact path="/become-a-sitter" />
+            <PrivateRoute exact path="/become-a-sitter" component={BecomeASitter} />
             <PrivateRoute exact path="/my-sitters" />
             <PrivateRoute exact path="/my-jobs" />
             <PrivateRoute exact path="/messages" />

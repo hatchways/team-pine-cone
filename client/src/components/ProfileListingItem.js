43d77c9ba382b7c7bedede5580e47cdff49b954f
Michael Braga <michael_braga@outlook.com>
2020-10-21 23:23:32 -0400

    Added animation to ProfileDetail, added uitility scroll to top hook

---
+++ client/src/components/ProfileListingItem.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Grid, Grow, Avatar, Typography } from "@material-ui/core";
+import { Rating } from "@material-ui/lab";
+import RoomIcon from "@material-ui/icons/Room";
+
+import { useStyle } from "../pages/ProfileListings";
+
+const Item = function (props) {
+  const {
+    src,
+    firstName,
+    lastName,
+    shortDescription,
+    rating,
+    hourlyRate,
+    location,
+  } = props;
+  const classes = useStyle();
+  const history = useHistory();
+
+  // go to individual listing from here
+  const handleClick = () => history.push("/profile-details/:id");
+
+  return (
+    <Grid item xs={12} className={classes.card} onClick={handleClick}>
+      <Grow in={true}>
+        <Grid container spacing={2} direction="column" alignItems="center">
+          <Grid item>
+            <Avatar className={classes.avatar} src={src} alt={lastName} />
+          </Grid>
+          <Grid item>
+            <Grid container alignItems="center" direction="column">
+              {/*fullName*/}
+              <Grid item id="fullName">
+                <Typography cvariant="h5" className={classes.bold}>
+                  {firstName + " " + lastName}
+                </Typography>
+              </Grid>
+              {/*title*/}
+              <Grid item id="title">
+                <Typography variant="body1" className={classes.subtile}>
+                  Loving Cat Sitter
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Rating value={rating} name="read-only" readOnly />
+          </Grid>
+          <Grid item id="shortDescription">
+            {/*shortDescription*/}
+            <Typography variant="body1" className={classes.description}>
+              {shortDescription}
+            </Typography>
+          </Grid>
+          <Grid item className={classes.cardBottom}>
+            <Grid container justify="space-around">
+              <Grid item>
+                <Grid container spacing={1}>
+                  <Grid item>
+                    <RoomIcon color="primary" />
+                  </Grid>
+                  {/*location*/}
+                  <Grid item id="location">
+                    <Typography className={classes.subtile}>
+                      {location}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+              {/*hourlyRate*/}
+              <Grid item id="hourlyRate">
+                <Typography variant="body1" className={classes.bold}>
+                  {hourlyRate}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grow>
+    </Grid>
+  );
+};
+
+export default Item;
--- client/src/pages/ProfileDetails.js
+++ client/src/pages/ProfileDetails.js
@@ -2,11 +2,12 @@ import "date-fns";
 import { addHours } from "date-fns";
 import React, { useState } from "react";
 import DateFnsUtils from "@date-io/date-fns";
-import { Grid, Avatar, Typography, Button } from "@material-ui/core";
+import { Grid, Avatar, Typography, Button, Grow } from "@material-ui/core";
 import { Rating } from "@material-ui/lab";
 import { makeStyles } from "@material-ui/core/styles";
 import RoomIcon from "@material-ui/icons/Room";
 import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import useScrollToTop from '../hooks/useScrollToTop';
 
 export const useStyles = makeStyles((theme) => ({
   root: {
@@ -69,6 +70,7 @@ const ProfileDetails = function () {
   const [selectDropIn, setSelectDropIn] = useState(addHours(new Date(), 1));
   const [selectDropOff, setSelectDropOff] = useState(addHours(new Date(), 2));
   const classes = useStyles();
+	useScrollToTop();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -77,141 +79,152 @@ const ProfileDetails = function () {
   };
 
   return (
-    <Grid className={classes.root} container>
-      <Grid item md={7}>
-        <Grid direction="column" container>
-          <Grid item>
-            <div className={classes.banner}></div>
-          </Grid>
-          <Grid item direction="column" container alignItems="center">
+    <Grow in={true}>
+      <Grid className={classes.root} container>
+        <Grid item md={7}>
+          <Grid direction="column" container>
             <Grid item>
-              <Avatar
-                className={classes.avatar}
-                src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-256.png"
-                alt="User name"
-              />
+              <div className={classes.banner}></div>
             </Grid>
-            <Grid item>
-              <Typography className={classes.name} variant="h4">
-                Michael Braga
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="body1" className={classes.subtile} paragraph>
-                Loving Cat Sitter
-              </Typography>
-            </Grid>
-            <Grid container spacing={2} justify="center" alignItems="center">
+            <Grid item direction="column" container alignItems="center">
               <Grid item>
-                <RoomIcon fontSize="large" color="primary" />
+                <Avatar
+                  className={classes.avatar}
+                  src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-256.png"
+                  alt="User name"
+                />
               </Grid>
               <Grid item>
-                <Typography className={classes.subtile}>
-                  Toronto, Ontario
+                <Typography className={classes.name} variant="h4">
+                  Michael Braga
                 </Typography>
               </Grid>
+              <Grid item>
+                <Typography
+                  variant="body1"
+                  className={classes.subtile}
+                  paragraph
+                >
+                  Loving Cat Sitter
+                </Typography>
+              </Grid>
+              <Grid container spacing={2} justify="center" alignItems="center">
+                <Grid item>
+                  <RoomIcon fontSize="large" color="primary" />
+                </Grid>
+                <Grid item>
+                  <Typography className={classes.subtile}>
+                    Toronto, Ontario
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid container className={classes.about}>
+              <Grid item>
+                <Typography className={classes.name} gutterBottom variant="h5">
+                  About me
+                </Typography>
+              </Grid>
+              <Grid item className={classes.mb2}>
+                <Typography paragraph gutterBottom>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Sit amet nulla facilisi morbi tempus. Tortor dignissim
+                  convallis aenean et tortor at. Odio euismod lacinia at quis
+                  risus sed vulputate odio. Venenatis tellus in metus vulputate
+                  eu. Duis convallis convallis tellus id interdum. Sapien eget
+                  mi proin sed. Risus nec feugiat in fermentum posuere urna. Dis
+                  parturient montes nascetur ridiculus mus mauris vitae
+                  ultricies. Semper risus in hendrerit gravida rutrum quisque
+                  non tellus orci. Faucibus et molestie ac feugiat sed. Cras
+                  semper auctor neque vitae tempus quam pellentesque.
+                </Typography>
+              </Grid>
+              <Grid container>
+                {[SD, SD, SD, SD].map((url, i) => (
+                  <Grid item key={url + i} xs={6} sm={3}>
+                    <img
+                      className={classes.pet}
+                      src={url}
+                      alt={"Pet " + (i + 1)}
+                    />
+                  </Grid>
+                ))}
+              </Grid>
             </Grid>
           </Grid>
-          <Grid container className={classes.about}>
+        </Grid>
+
+        {/*Drop Form*/}
+        <Grid
+          onSubmit={handleSubmit}
+          component="form"
+          className={classes.dropInfo}
+          item
+          md={5}
+        >
+          <Grid
+            container
+            className={classes.mb2}
+            alignItems="center"
+            direction="column"
+          >
             <Grid item>
-              <Typography className={classes.name} gutterBottom variant="h5">
-                About me
+              <Typography paragraph variant="h4">
+                $14/hr
               </Typography>
             </Grid>
-            <Grid item className={classes.mb2}>
-              <Typography paragraph gutterBottom>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit
-                amet nulla facilisi morbi tempus. Tortor dignissim convallis
-                aenean et tortor at. Odio euismod lacinia at quis risus sed
-                vulputate odio. Venenatis tellus in metus vulputate eu. Duis
-                convallis convallis tellus id interdum. Sapien eget mi proin
-                sed. Risus nec feugiat in fermentum posuere urna. Dis parturient
-                montes nascetur ridiculus mus mauris vitae ultricies. Semper
-                risus in hendrerit gravida rutrum quisque non tellus orci.
-                Faucibus et molestie ac feugiat sed. Cras semper auctor neque
-                vitae tempus quam pellentesque.
-              </Typography>
+            <Grid item>
+              <Rating value={4} name="read-only" readOnly />
             </Grid>
-            <Grid container>
-              {[SD, SD, SD, SD].map((url, i) => (
-                <Grid item key={url + i} xs={6} sm={3}>
-                  <img
-                    className={classes.pet}
-                    src={url}
-                    alt={"Pet " + (i + 1)}
+          </Grid>
+          <Grid item className={classes.mb3}>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <Grid
+                container
+                alignItems="center"
+                direction="column"
+                spacing={2}
+              >
+                <Grid item xs={6}>
+                  <DateTimePicker
+                    strictCompareDates
+                    disablePast
+                    fullWidth
+                    value={selectDropIn}
+                    onChange={setSelectDropIn}
+                    label="Drop In"
+                    inputVariant="outlined"
                   />
                 </Grid>
-              ))}
-            </Grid>
+                <Grid item xs={6}>
+                  <DateTimePicker
+                    disablePast
+                    strictCompareDates
+                    fullWidth
+                    value={selectDropOff}
+                    onChange={setSelectDropOff}
+                    label="Drop Off"
+                    inputVariant="outlined"
+                  />
+                </Grid>
+              </Grid>
+            </MuiPickersUtilsProvider>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              size="large"
+              type="submit"
+              color="primary"
+              fullWidth
+            >
+              Send Request
+            </Button>
           </Grid>
         </Grid>
       </Grid>
-
-      {/*Drop Form*/}
-      <Grid
-        onSubmit={handleSubmit}
-        component="form"
-        className={classes.dropInfo}
-        item
-        md={5}
-      >
-        <Grid
-          container
-          className={classes.mb2}
-          alignItems="center"
-          direction="column"
-        >
-          <Grid item>
-            <Typography paragraph variant="h4">
-              $14/hr
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Rating value={4} name="read-only" readOnly />
-          </Grid>
-        </Grid>
-        <Grid item className={classes.mb3}>
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container alignItems="center" direction="column" spacing={2}>
-              <Grid item xs={6}>
-                <DateTimePicker
-                  strictCompareDates
-                  disablePast
-                  fullWidth
-                  value={selectDropIn}
-                  onChange={setSelectDropIn}
-                  label="Drop In"
-                  inputVariant="outlined"
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <DateTimePicker
-                  disablePast
-                  strictCompareDates
-                  fullWidth
-                  value={selectDropOff}
-                  onChange={setSelectDropOff}
-                  label="Drop Off"
-                  inputVariant="outlined"
-                />
-              </Grid>
-            </Grid>
-          </MuiPickersUtilsProvider>
-        </Grid>
-        <Grid item>
-          <Button
-            variant="contained"
-            size="large"
-            type="submit"
-            color="primary"
-            fullWidth
-          >
-            Send Request
-          </Button>
-        </Grid>
-      </Grid>
-    </Grid>
+    </Grow>
   );
 };
 

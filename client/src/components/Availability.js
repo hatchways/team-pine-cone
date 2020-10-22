0b1361c34ac9f7b8930c948163487e1ba5c5e952
Dan Proctor <dproc96@gmail.com>
2020-10-22 18:18:49 -0400

    Update Availability.js
    Added basic availability skeleton

--- client/src/components/Availability.js
+++ client/src/components/Availability.js
@@ -1,10 +1,31 @@
+import DateFnsUtils from '@date-io/date-fns';
+import { Grid, Typography } from '@material-ui/core';
+import { DateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import React from 'react';
 
 function Availability(props) {
     return (
-        <div>
-            Availability
-        </div>
+      <Grid component="form" container direction="column" alignItems="center">
+        <Grid item>
+          <Typography variant="h2">Availability</Typography>
+        </Grid>
+        <Grid
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+          item
+        >
+          <Typography variant="p">Availability Range 1</Typography>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <DateTimePicker label="Start" />
+            <Typography variant="p">to</Typography>
+            <DateTimePicker label="End" />
+          </MuiPickersUtilsProvider>
+        </Grid>
+      </Grid>
     );
 }
 

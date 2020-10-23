80fa2e686c72afe7f383ce0cfbf9ca8dca6a90b7
Michael Braga <michael_braga@outlook.com>
2020-10-23 16:08:42 -0400

    Updates

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,20 @@ import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 
 import "./App.css";
+import PrivateRoute from "./components/PrivateRoute/";
 import Navbar from './components/Navbar';
 import ProfileSettings from "./pages/ProfileSettings";
 import ProfileDetails from "./pages/ProfileDetails";
+import ProfileListings from './pages/ProfileListings';
+import BecomeASitter from "./pages/BecomeASitter";
 
 import { AuthProvider } from "./contexts/user";
-import PrivateRoute from "./components/PrivateRoute/";
+import { ProfileProvider } from "./contexts/profile";
 import { STRIPE_PUBLIC_KEY } from "./data/stripe";
+
 
 const stripe = loadStripe(STRIPE_PUBLIC_KEY);
 
-import ProfileListings from './pages/ProfileListings';
-
-import PrivateRoute from "./components/PrivateRoute/";
-import { ProfileProvider } from "./contexts/profile";
-import BecomeASitter from "./pages/BecomeASitter";
 
 function App() {
   return (

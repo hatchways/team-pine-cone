7b0c407b79d44a66d6a9ea10467cb04717c9dc5b
Dan Proctor <dproc96@gmail.com>
2020-10-24 13:52:57 -0400

    Update profile.js
    Changed to functional component

--- client/src/contexts/profile.js
+++ client/src/contexts/profile.js
@@ -1,43 +1,56 @@
-import React, {useContext, createContext } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, {
+  useContext,
+  createContext,
+  useState,
+  useEffect,
+  useRef,
+} from "react";
+import { useLocation, withRouter } from "react-router-dom";
 
 export const ProfileContext = createContext(null);
 
 export const useProfileContext = () => {
-    const profileContext = useContext(ProfileContext);
-    return profileContext;
-}
+  const profileContext = useContext(ProfileContext);
+  return profileContext;
+};
 
-class ProfileProvider extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            profile: null,
-            setProfile: profile => { this.setState({profile}) }
-        }
+function ProfileProvider(props) {
+  const [profile, setProfile] = useState(null);
+  const firstUpdate = useRef(true);
+  const prevLocation = useRef(null);
+  const context = {
+    profile,
+    setProfile: (profileUpdate) => {
+      const newProfile = { ...profileUpdate };
+      setProfile(newProfile);
+    },
+  };
+  const location = useLocation().pathname;
+  useEffect(() => {
+    if (firstUpdate.current) {
+      fetch("/profile/me").then((profile) => {
+        profile.json().then((result) => {
+          setProfile(result);
+        });
+      });
+      firstUpdate.current = false;
+    } else {
+      const hasLoggedIn = (prevLocation.current === "/login" || prevLocation.current === "/signup") && (location !== "/login" || location !== "/signup");
+      if (hasLoggedIn) {
+        fetch("/profile/me").then((profile) => {
+          profile.json().then((result) => {
+            setProfile(result);
+          });
+        });
+      }
     }
-    componentDidMount() {
-        fetch("/profile/me").then(profile => {
-            profile.json().then(result => {
-                this.state.setProfile(result);
-            })
-        })
-    }
-    componentDidUpdate(prevProps) {
-        const hasLoggedIn = (prevProps.location.pathname === "/login" || prevProps.location.pathname === "/signup") && (this.props.location.pathname !== "/login" || this.props.location.pathname !== "/signup")
-        if (hasLoggedIn) {
-            fetch("/profile/me").then((profile) => {
-              profile.json().then((result) => {
-                this.state.setProfile(result);
-              });
-            });
-        }
-    }
-    render() {
-        return (
-            <ProfileContext.Provider value={this.state}>{this.props.children}</ProfileContext.Provider>
-        )
-    }
+    prevLocation.current = location;
+  });
+  return (
+    <ProfileContext.Provider value={context}>
+      {props.children}
+    </ProfileContext.Provider>
+  );
 }
 
 export default withRouter(ProfileProvider);

ae9a90bda5e8d69e4ecd4521497f503f0dddc8e1
Dan Proctor <dproc96@gmail.com>
2020-10-26 19:12:24 -0400

    Update Bookings.js
    Built out skeleton of Bookings page

--- client/src/components/Bookings.js
+++ client/src/components/Bookings.js
@@ -1,12 +1,36 @@
-import { Card, Toolbar } from '@material-ui/core';
+import { Card, makeStyles, Toolbar } from '@material-ui/core';
 import React, { Fragment } from 'react';
 
+const useStyles = makeStyles({
+    mainCard: {
+        padding: 10,
+        textAlign: "center",
+        margin: 20
+    },
+    subCard: {
+        display: "grid",
+        gridAutoColumns: "auto",
+        padding: 10,
+    }
+})
+
 function Bookings({ isMyJobs }) {
+    const classes = useStyles()
     return (
         <Fragment>
             <Toolbar />
-            <Card>
-                <h2>{isMyJobs ? "My Jobs" : "My Sitters"}</h2>
+            <Card className={classes.mainCard}>
+                <h1>{isMyJobs ? "My Jobs" : "My Sitters"}</h1>
+                <h2>Upcoming Bookings</h2>
+                <Card variant="outlined" className={classes.subCard}>
+                    Booking
+                </Card>
+                <p>No upcoming bookings</p>
+                <h2>Pending Requests</h2>
+                <Card variant="outlined" className={classes.subCard}>
+                    Request
+                </Card>
+                <p>No pending requests</p>
             </Card>
         </Fragment>
     );

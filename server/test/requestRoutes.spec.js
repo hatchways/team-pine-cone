a05ae938f70a0bdf889614ad6c3f0b291305c263
Dan Proctor <dproc96@gmail.com>
2020-10-25 22:12:31 -0400

    Update requestRoutes.spec.js
    Set up User log in before testing

--- server/test/requestRoutes.spec.js
+++ server/test/requestRoutes.spec.js
@@ -1,36 +1,61 @@
+const { IdentityStore } = require("aws-sdk");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const app = require("../app.js");
-const { Profile } = require("../models")
+const { Profile, User } = require("../models");
 
 const { expect } = chai;
 
-chai.should()
-chai.use(chaiHttp)
+chai.should();
+chai.use(chaiHttp);
+
+const testingIds = [];
 
 describe("Request Routes", () => {
-    before(async () => {
-        const testProfile = new db.Profile({
-          firstName: "Joe",
-          lastName: "Smith",
-          birthDate: new Date("01-01-1990"),
-          phone: "+1 (555) 555-5555",
-          location: {
-            type: "Point",
-            coordinates: [-122.5, 37.7],
-          },
+  before(async done => {
+    const testProfile = new Profile({
+      firstName: "Joe",
+      lastName: "Smith",
+      birthDate: new Date("01-01-1990"),
+      phone: "+1 (555) 555-5555",
+      location: {
+        type: "Point",
+        coordinates: [-122.5, 37.7],
+      },
+    });
+    testProfile.save().then(profile => {
+      testingIds.push(profile._id);
+      const testProfile2 = new Profile({
+        firstName: "Jim",
+        lastName: "Smith",
+        birthDate: new Date("01-01-1990"),
+        phone: "+1 (555) 555-5555",
+        location: {
+          type: "Point",
+          coordinates: [-122.5, 37.7],
+        },
+      });
+      testProfile2.save().then(profile => {
+        testingIds.push(profile._id);
+        const testUser = new User({
+          email: "joe@test.com",
+          password: "joetest",
+          profile: testingIds[0]
         });
-        testProfile2.save();
-        const testProfile = new db.Profile({
-          firstName: "Joe",
-          lastName: "Smith",
-          birthDate: new Date("01-01-1990"),
-          phone: "+1 (555) 555-5555",
-          location: {
-            type: "Point",
-            coordinates: [-122.5, 37.7],
-          },
+        testUser.save().then(() => {
+          chai
+            .request(app)
+            .post("/login")
+            .send({
+              email: testUser.email,
+              password: testUser.password
+            })
+            .end(res => {
+              res.should.have.status(200);
+              done();
+            });
         });
-        testProfile2.save();
-    })
-})
+      });
+    });
+  });
+});

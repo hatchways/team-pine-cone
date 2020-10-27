1b262dc7e3361aae2650c1750e61d3ef117f0358
Dan Proctor <dproc96@gmail.com>
2020-10-27 19:34:58 -0400

    Update socket.js

--- server/utils/socket.js
+++ server/utils/socket.js
@@ -1,3 +1,11 @@
-module.exports = io => {
-    
-}
+let io;
+
+exports.create = server => {
+  io = require("socket.io")(server);
+};
+
+io.on("connection", socket => {
+  console.log(socket);
+});
+
+exports.io = io;

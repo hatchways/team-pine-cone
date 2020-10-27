36bcd09a6ca9792e0228753e52ab991c1593c185
Dan Proctor <dproc96@gmail.com>
2020-10-26 20:06:21 -0400

    Update Bookings.js
    Hooked in profile context

--- client/src/components/Bookings.js
+++ client/src/components/Bookings.js
@@ -1,5 +1,6 @@
 import { Card, makeStyles, Toolbar } from '@material-ui/core';
 import React, { Fragment } from 'react';
+import { useProfileContext } from '../contexts/profile';
 import Subcard from './Subcard';
 
 const useStyles = makeStyles({
@@ -16,25 +17,40 @@ const useStyles = makeStyles({
 
 function Bookings({ isMyJobs }) {
     const classes = useStyles()
-    const subcardDummyData = {
-        photoSrc: "https://team-pine-cone.s3.amazonaws.com/26233331_1999431303611867_6513675455554858596_o-Mon%20Oct%2026%202020%2018%3A37%3A06%20GMT-0400%20%28Eastern%20Daylight%20Time%29.jpg",
-        name: "Dan Proctor",
-        start: new Date(),
-        end: new Date()
-    }
+    const { profile } = useProfileContext()
+    const bookings = profile.requests.filter(
+      (request) =>
+        ((isMyJobs && request.sitter_id === profile._id) ||
+          request.user_id === profile._id) &&
+        request.accepted
+    );
+    const requests = profile.requests.filter(
+      (request) =>
+        ((isMyJobs && request.sitter_id === profile._id) ||
+          request.user_id === profile._id) &&
+        !request.accepted && !request.declined
+    );
     return (
-        <Fragment>
-            <Toolbar />
-            <Card className={classes.mainCard}>
-                <h1>{isMyJobs ? "My Jobs" : "My Sitters"}</h1>
-                <h2>Upcoming Bookings</h2>
-                <Subcard {...subcardDummyData} isBooking={true} />
-                <p className={classes.emptyText}>No upcoming bookings</p>
-                <h2>Pending Requests</h2>
-                <Subcard {...subcardDummyData} />
-                <p className={classes.emptyText}>No pending requests</p>
-            </Card>
-        </Fragment>
+      <Fragment>
+        <Toolbar />
+        <Card className={classes.mainCard}>
+          <h1>{isMyJobs ? "My Jobs" : "My Sitters"}</h1>
+          <h2>Upcoming Bookings</h2>
+          {bookings.map((booking) => (
+            <Subcard isMyJobs={isMyJobs} {...booking} isBooking={true} />
+          ))}
+          {bookings.length === 0 && (
+            <p className={classes.emptyText}>No upcoming bookings</p>
+          )}
+          <h2>Pending Requests</h2>
+          {requests.map((request) => (
+            <Subcard isMyJobs={isMyJobs} {...request} />
+          ))}
+          {requests.length === 0 && (
+            <p className={classes.emptyText}>No pending requests</p>
+          )}
+        </Card>
+      </Fragment>
     );
 }
 

099c4065ca860853773b4101dc823e25ccce478f
Dan Proctor <dproc96@gmail.com>
2020-10-26 21:28:52 -0400

    Update Booking.js

--- client/src/components/Booking.js
+++ client/src/components/Booking.js
@@ -1,14 +1,16 @@
 import { Avatar, Button, Card, makeStyles } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
+import { useProfileContext } from "../contexts/profile";
 
 const useStyles = makeStyles({
-  Booking: {
+  booking: {
     display: "grid",
     gridAutoColumns: "auto",
     gridAutoRows: "auto",
     padding: 20,
     alignItems: "center",
     textAlign: "left",
+    marginTop: 10
   },
   photo: {
     gridColumn: "1 / span 1",
@@ -34,32 +36,76 @@ const useStyles = makeStyles({
   },
 });
 
-const formatDate = date => (
-    `${date.getMonth()}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`
+const formatDate = dateStr => {
+  const date = new Date(dateStr)
+  return `${doubleNumber(date.getMonth() + 1)}/${doubleNumber(date.getDate())} ${doubleNumber(date.getHours())}:${doubleNumber(date.getMinutes())}`
+}
+
+const doubleNumber = number => (
+  number < 10 ? `0${number}` : number
 )
 
-function Booking({ isBooking, isMyJobs, sitter_id, user_id, start, end, paid }) {
+function Booking({ _id, isBooking, isMyJobs, sitter_id, user_id, start, end, paid }) {
   const classes = useStyles();
+  const { profile, setProfile } = useProfileContext();
   const [src, setSrc] = useState(null);
   const [name, setName] = useState("");
   useEffect(() => {
       const id = isMyJobs ? user_id : sitter_id
       fetch(`/profile/${id}`).then(response => {
-          setSrc(response.body.photo);
-          setName(`${response.body.firstName} ${response.body.lastName}`);
+        response.json().then(profile => {
+          setSrc(profile.photo);
+          setName(`${profile.firstName} ${profile.lastName}`);
+        })
       })
   },[setSrc, isMyJobs, sitter_id, user_id])
+  const handleAccept = () => {
+    handleAcceptOrDecline(true)
+  }
+  const handleDecline = () => {
+    handleAcceptOrDecline(false)
+  }
+  const handleAcceptOrDecline = accept => {
+    const newProfile = {...profile}
+    let updatedRequest
+    newProfile.requests.forEach(request => {
+      if (request._id === _id) {
+        request.accepted = accept
+        request.declined = !accept
+        updatedRequest = request
+      }
+    })
+    setProfile(newProfile)
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedRequest),
+    };
+    fetch(`/request/update/${updatedRequest._id}`, options);
+  }
   return (
-    <Card variant="outlined" className={classes.Booking}>
+    <Card variant="outlined" className={classes.booking}>
       <Avatar src={src} className={classes.photo} />
       <h2 className={classes.name}>{name}</h2>
       <h3 className={classes.date}>
         {formatDate(start)} - {formatDate(end)}
       </h3>
-      <Button color="primary" variant="contained" className={classes.button1}>
+      <Button
+        onClick={isBooking ? null : handleAccept}
+        color="primary"
+        variant="contained"
+        className={classes.button1}
+      >
         {isBooking ? "Message" : "Accept"}
       </Button>
-      <Button color="primary" variant="contained" className={classes.button2}>
+      <Button
+        onClick={isBooking ? null : handleDecline}
+        color="primary"
+        variant="contained"
+        className={classes.button2}
+      >
         {isBooking ? (paid ? "Cancel" : "Pay") : "Decline"}
       </Button>
       {isBooking && !paid && (

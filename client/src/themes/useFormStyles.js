1d583711c57c5cd7fc85b92bd6cb0e43dddac5f0
Dan Proctor <dproc96@gmail.com>
2020-10-27 20:38:46 -0400

    Update useFormStyles.js
    Added bottom margin

--- client/src/themes/useFormStyles.js
+++ client/src/themes/useFormStyles.js
@@ -3,10 +3,11 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(5),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    padding: theme.spacing(5)
+    padding: theme.spacing(5),
   },
   form: {
     width: "100%", // Fix IE 11 issue.

c3ded539b078a9d40e98294e979274ac717b16d8
Dan Proctor <dproc96@gmail.com>
2020-10-27 20:40:53 -0400

    Revert "Update Login.js"
    This reverts commit 4f4436f3969bbf7075da275a8ba8d71c45f4f207.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,7 +8,7 @@ import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import { Card, Toolbar } from "@material-ui/core";
+import { Toolbar } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
 import useForm from "../components/useForm";
 import Input from "../components/controls/Input";
@@ -63,7 +63,7 @@ export default function SignIn() {
     <Fragment>
       <Toolbar />
       <Container component="main" maxWidth="xs">
-        <Card className={classes.paper}>
+        <div className={classes.paper}>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
@@ -129,8 +129,10 @@ export default function SignIn() {
               </Grid>
             </Grid>
           </form>
+        </div>
+        <Box mt={8}>
           <Copyright />
-        </Card>
+        </Box>
       </Container>
     </Fragment>
   ) : (

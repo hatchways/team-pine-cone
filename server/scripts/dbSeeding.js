0a7464aa189b052d13471407e255fc73714e77d0
Michael Braga <michael_braga@outlook.com>
2020-10-27 20:52:47 -0400

    Profiles successful seeds into database

--- server/scripts/dbSeeding.js
+++ server/scripts/dbSeeding.js
@@ -3,47 +3,86 @@ require("dotenv").config();
 const { Profile } = require("../models/");
 const mongodbUri = process.env.MONGODB_URI;
 const mongoose = require("mongoose");
-const { name, random, date: fakerDate, lorem, phone, image } = require("faker/locale/en_CA");
+const { addMonths, subYears } = require("date-fns");
+const {
+  name,
+  random,
+  date: fakerDate,
+  lorem,
+  phone,
+  image,
+  address,
+} = require("faker/locale/en_CA");
 
 const genders = Profile.schema.paths.gender.enumValues;
 
-const createBirthdate = subYear => new Date(new Date().getFullYear() - subYear, 1, 1);
-const minDate = createBirthdate(19);
-const maxDate = createBirthdate(90);
-
-//const createDate = month => new Date(new Date().getFullYear, );
-const nearestMonthDate = () => {
-	const today = new Date();
-	return fakerDate.between(today, new Date(today.getFullYear, 11, 1));
+const randomDateBetweenNowAndAMonth = () => {
+  const today = new Date();
+  return fakerDate.between(today, addMonths(new Date(), 1));
 };
 
+const Toronto = [-79.447256, 43.6824164];
+const QuebecCity = [-71.2074252, 46.8141835];
+const Montreal = [-73.553363, 45.509062];
+const Calgary = [-114.05803, 51.046362];
+
+const mainCitiesGPS = [Toronto, QuebecCity, Montreal, Calgary];
+
 (async function () {
-	/*
-  const connection = await mongoose.connect(mongodbUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
-  */
+  try {
+    let profiles = [];
 
-	let profiles = [];
+    const connection = await mongoose.connect(mongodbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
 
-	for (let i = 0; i < 15; i++) { 
-		const user = { 
-			firstName: name.firstName(),
-			lastName: name.lastName(),
-			gender: random.arrayElement(genders),
-			birthDate: fakerDate.between(minDate, maxDate),
-			description: lorem.paragraph(),
-			isSitter: true,
-			
-			phone: phone.phoneNumber("+1 (!##) !##-####"),
-			photo: image.avatar(),
-		};
+    for (let i = 0; i < 15; i++) {
+      let availabilityStart = randomDateBetweenNowAndAMonth();
+      let availabilityEnd = randomDateBetweenNowAndAMonth();
+      const coordinates = random.arrayElement(mainCitiesGPS);
+      const randomBirthDate = fakerDate.between(
+        subYears(new Date(), 19),
+        subYears(new Date(), 90)
+      );
 
-		profiles.push(user);
-	}
+      if (availabilityStart > availabilityEnd) {
+        let temp = availabilityEnd;
+        availabilityEnd = availabilityStart;
+        availabilityStart = temp;
+      }
 
-	console.log(nearestMonthDate())
+      const user = {
+        firstName: name.firstName(),
+        lastName: name.lastName(),
+        gender: random.arrayElement(genders),
+        birthDate: randomBirthDate,
+        description: lorem.paragraph(),
+        isSitter: true,
+        availability: [
+          {
+            start: availabilityStart,
+            end: availabilityEnd,
+          },
+        ],
+        location: {
+          type: "Point",
+          coordinates: coordinates,
+        },
+        phone: phone.phoneNumber("+1 (!##) !##-####"),
+        photo: image.avatar(),
+      };
 
+      profiles.push(user);
+    }
+
+    await Profile.insertMany(profiles);
+
+    console.log("Seeding successful");
+    process.exit();
+  } catch (err) {
+    console.log(err.message);
+    console.log("Seeding profiles failed");
+  }
 })();

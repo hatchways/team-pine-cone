b0df9d32131d23c6d8f5f325672dec0dfa5ada21
Dan Proctor <dproc96@gmail.com>
2020-10-28 09:17:20 -0400

    Update MyNotifications.js

--- client/src/components/MyNotifications.js
+++ client/src/components/MyNotifications.js
@@ -1,18 +1,15 @@
 import React from "react";
-import { Avatar, Card, Grid, makeStyles } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Grid, makeStyles } from "@material-ui/core";
+import Notification from "./Notification";
 
 const useStyles = makeStyles({
-  emptyText: {
-    fontStyle: "italic",
-    fontSize: 10,
-  },
   notification: {
     width: "95%",
     padding: "0px 20px",
   },
-  notificationText: {
-    textDecoration: "none",
+  emptyText: {
+    fontStyle: "italic",
+    fontSize: 10,
   },
 });
 
@@ -24,19 +21,7 @@ function MyNotifications(props) {
         <h2>Notifications</h2>
       </Grid>
       <Grid className={classes.notification} item>
-        <Link className={classes.notificationText} to="/my-jobs">
-          <Card className={classes.notification} variant="outlined">
-            <Grid container direction="row" alignItems="center">
-              <Grid style={{marginRight: 20}} item>
-                <Avatar />
-              </Grid>
-              <Grid item>
-                <h3>Title</h3>
-                <p>Description</p>
-              </Grid>
-            </Grid>
-          </Card>
-        </Link>
+        <Notification title="Title" description="Description" link="/my-jobs" src={null} />
       </Grid>
       <Grid item>
         <p className={classes.emptyText}>Looks like you're all caught up!</p>

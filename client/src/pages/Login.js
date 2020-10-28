4f4436f3969bbf7075da275a8ba8d71c45f4f207
Dan Proctor <dproc96@gmail.com>
2020-10-27 20:38:54 -0400

    Update Login.js
    Swapped div for Card

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,7 +8,7 @@ import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import { Toolbar } from "@material-ui/core";
+import { Card, Toolbar } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
 import useForm from "../components/useForm";
 import Input from "../components/controls/Input";
@@ -63,7 +63,7 @@ export default function SignIn() {
     <Fragment>
       <Toolbar />
       <Container component="main" maxWidth="xs">
-        <div className={classes.paper}>
+        <Card className={classes.paper}>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
@@ -129,10 +129,8 @@ export default function SignIn() {
               </Grid>
             </Grid>
           </form>
-        </div>
-        <Box mt={8}>
           <Copyright />
-        </Box>
+        </Card>
       </Container>
     </Fragment>
   ) : (

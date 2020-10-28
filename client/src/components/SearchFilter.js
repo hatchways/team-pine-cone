62daeeb1de99e61e4b2f22d2a16e0908a8d979e1
Michael Braga <michael_braga@outlook.com>
2020-10-28 15:43:23 -0400

    Working on filter options

--- client/src/components/SearchFilter.js
+++ client/src/components/SearchFilter.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "date-fns";
+import DateFnsUtils from "@date-io/date-fns";
 import { makeStyles } from "@material-ui/styles/";
 import SearchIcon from '@material-ui/icons/Search';
 import AddIcon from '@material-ui/icons/Add';
-import { Grid, FormControl, InputLabel, Input, InputBase, Button, TextField, Fab } from "@material-ui/core/";
+import { Rating } from "@material-ui/lab/";
+import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import useForm from "./useForm";
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, FormControl, InputLabel, Input, InputBase, Button, TextField, Fab, Slider, Switch } from "@material-ui/core/";
 
 const useStyle = makeStyles(theme => ({
 	container: { 
@@ -41,11 +46,25 @@ const useStyle = makeStyles(theme => ({
 	fab: {
 		marginLeft: "auto",
 		transition: "none"
-	}
+	},
 }));
+
+const form = {
+	rating: 0,
+	price: [0, 300],
+	date: new Date()
+};
+
+const valuePriceText = value => `$${value}`;
 
 const SearchFilter = function() {
 	const classes = useStyle();
+	const [isDialogOpen, setIsDialogOpen] = useState(true);
+	const { values, handleInputChange, setValues, handleDateChange } = useForm(form);
+
+	const handleSliders = (prop) => (e, newValue) => {
+		setValues({...values, [prop]: newValue});
+	};
 
 	return (
 		<Grid container component="form" className={classes.container}>
@@ -53,9 +72,34 @@ const SearchFilter = function() {
 				<SearchIcon className={classes.icon} fontSize="large" />
 				<TextField label="Search" />
 			</Grid>
-			<Fab color="primary" size="small" className={classes.fab}>
+			<Fab color="primary" size="small" className={classes.fab} onClick={() => setIsDialogOpen(true)}>
 				<AddIcon />
 			</Fab>
+
+			{ /*Filters*/ }
+			<Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)} aria-labelledby="filters-dialog-title">
+				<DialogTitle id="filters-dialog-title">Filters</DialogTitle>
+				<DialogContent>
+					<DialogContentText>
+						Ratings
+					</DialogContentText>
+					<Rating name="sitter-ratings" defaultValue={0} onChange={handleInputChange} values={values.rating} size="large"/>
+					<DialogContentText>
+						($) Price
+					</DialogContentText>
+					<Slider min={0} max={300} name="price-range" value={values.price} onChange={handleSliders("price")}  valueLabelDisplay="auto" aria-labelledby="price-slider" getAriaValueText={valuePriceText}/>
+					<DialogContentText>
+						Availability
+					</DialogContentText>
+					<MuiPickersUtilsProvider utils={DateFnsUtils}>
+						<DateTimePicker label="From" value={values.date} disablePast onChange={handleDateChange} showTodayButton/>
+						<DateTimePicker label="To" value={values.date} disablePast onChange={handleDateChange} showTodayButton/>
+					</MuiPickersUtilsProvider>
+					<DialogContentText>
+						Sort By
+					</DialogContentText>
+				</DialogContent>
+			</Dialog>
 		</Grid>
 	);
 }

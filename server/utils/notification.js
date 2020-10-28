cb63fa956a097234829e6bcb20a2161a104135be
Dan Proctor <dproc96@gmail.com>
2020-10-28 07:40:45 -0400

    Create notification.js

---
+++ server/utils/notification.js
@@ -0,0 +1,19 @@
+const Profile = require("../models/Profile");
+const socket = require("./socket");
+
+exports.notify = (id, notification) => {
+  Profile.findById(id).then(profile => {
+    profile.notifications.push(notification);
+    socket.io.to(id.toString()).emit(notification);
+  });
+};
+
+exports.remove = (profileId, notificationId) => {
+  Profile.findById(profileId).then(profile => {
+    let index;
+    profile.notifications.forEach((notification, i) => {
+      if (notification._id === notificationId) index = i;
+    });
+    if (index) profile.notifications.splice(index, 1);
+  });
+};

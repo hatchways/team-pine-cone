65531fc6a504d73624492dd1388a7e99d86ae174
Dan Proctor <dproc96@gmail.com>
2020-10-28 22:03:31 -0400

    Update Notification.js

--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Avatar, Card, Grid, makeStyles } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { useProfileContext } from '../contexts/profile';
 
 const useStyles = makeStyles({
   notification: {
@@ -13,10 +14,27 @@ const useStyles = makeStyles({
   },
 });
 
-function Notification({ title, message, link, src }) {
+function Notification({ title, message, link, src, _id }) {
     const classes = useStyles()
+    const {profile, pullProfile} = useProfileContext();
+    const handleClick = () => {
+      let index;
+      profile.notifications.forEach((notification, i) => {
+        if (notification._id === _id) index = i
+      })
+      profile.notifications.splice(index, 1)
+      fetch(`/profile/${profile._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(profile),
+      }).then(() => {
+        pullProfile()
+      });
+    }
     return (
-      <Link className={classes.notificationText} to={link}>
+      <Link onClick={handleClick} className={classes.notificationText} to={link}>
         <Card className={classes.notification} variant="outlined">
           <Grid container direction="row" alignItems="center">
             <Grid style={{ marginRight: 20 }} item>

5f1ab695379ef938b79ae05aac7f4e0052d6e2c2
Michael Braga <michael_braga@outlook.com>
2020-10-28 20:59:58 -0400

    Updated filters

--- client/src/components/SearchFilter.js
+++ client/src/components/SearchFilter.js
@@ -1,107 +1,190 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "date-fns";
 import DateFnsUtils from "@date-io/date-fns";
 import { makeStyles } from "@material-ui/styles/";
-import SearchIcon from '@material-ui/icons/Search';
-import AddIcon from '@material-ui/icons/Add';
+import SearchIcon from "@material-ui/icons/Search";
+import AddIcon from "@material-ui/icons/Add";
 import { Rating } from "@material-ui/lab/";
 import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { addDays } from "date-fns";
 import useForm from "./useForm";
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, FormControl, InputLabel, Input, InputBase, Button, TextField, Fab, Slider, Switch } from "@material-ui/core/";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  RadioGroup,
+  Radio,
+  InputLabel,
+  Input,
+  InputBase,
+  Button,
+  TextField,
+  Fab,
+  Slider,
+  Switch,
+} from "@material-ui/core/";
 
-const useStyle = makeStyles(theme => ({
-	container: { 
-		width: "100%",
-		maxWidth: "400px",
-		margin: "auto",
-		borderRadius: "50px",
-		padding: "0.8em",
-		transition: "all 0.2s",
-		color: "white",
-		"&:hover": { 
-			background: theme.palette.primary.main,
-		},
-		"&:hover svg": { 
-			fill: theme.palette.primary.main,
-			opacity: 1,
-			background: "white"
-		},
-		"&:hover input": { 
-			color: "white",
-		},
-		"&:hover label": { 
-			color: "white",
-		},
-		"&:hover button": { 
-			background: "white",
-		},
-	},
-	icon: { 
-		marginRight: "10px",
-		background: theme.palette.primary.main,
-		fill: "white",
-		borderRadius: "50%",
-		padding: "0.2em",
-	},
-	fab: {
-		marginLeft: "auto",
-		transition: "none"
-	},
+const useStyle = makeStyles((theme) => ({
+  container: {
+    width: "100%",
+    maxWidth: "400px",
+    margin: "auto",
+    borderRadius: "50px",
+    padding: "0.8em",
+    transition: "all 0.3s",
+    color: "white",
+    "&:hover": {
+      background: theme.palette.primary.main,
+		opacity: 0.8
+    },
+    "&:hover svg": {
+      fill: theme.palette.primary.main,
+      opacity: 1,
+      background: "white",
+    },
+    "&:hover input": {
+      color: "white",
+    },
+    "&:hover label": {
+      color: "white",
+    },
+    "&:hover button": {
+      background: "white",
+    },
+  },
+  icon: {
+    marginRight: "10px",
+    background: theme.palette.primary.main,
+    fill: "white",
+    borderRadius: "50%",
+    padding: "0.2em",
+  },
+  fab: {
+    marginLeft: "auto",
+    transition: "none",
+  },
+  fromDate: {
+    marginRight: "1em",
+  },
+	title: { 
+		color: theme.palette.primary.main
+	}
 }));
 
 const form = {
-	rating: 0,
-	price: [0, 300],
-	date: new Date()
+  rating: 0,
+  price: [0, 300],
+  fromDate: null,
+  toDate: null,
+  sortBy: "sitters",
 };
 
-const valuePriceText = value => `$${value}`;
+const valuePriceText = (value) => `$${value}`;
 
-const SearchFilter = function() {
-	const classes = useStyle();
-	const [isDialogOpen, setIsDialogOpen] = useState(true);
-	const { values, handleInputChange, setValues, handleDateChange } = useForm(form);
+const SearchFilter = function () {
+  const classes = useStyle();
+  const [isDialogOpen, setIsDialogOpen] = useState(true);
+  const { values, handleInputChange, setValues, handleDateChange } = useForm(
+    form
+  );
 
-	const handleSliders = (prop) => (e, newValue) => {
-		setValues({...values, [prop]: newValue});
-	};
+  const handleSliders = (prop) => (e, newValue) => {
+    setValues({ ...values, [prop]: newValue });
+  };
 
-	return (
-		<Grid container component="form" className={classes.container}>
-			<Grid item>
-				<SearchIcon className={classes.icon} fontSize="large" />
-				<TextField label="Search" />
-			</Grid>
-			<Fab color="primary" size="small" className={classes.fab} onClick={() => setIsDialogOpen(true)}>
-				<AddIcon />
-			</Fab>
+  return (
+    <Grid container component="form" className={classes.container}>
+      <Grid item>
+        <SearchIcon className={classes.icon} fontSize="large" />
+        <TextField label="Search" />
+      </Grid>
+      <Fab
+        color="primary"
+        size="small"
+        className={classes.fab}
+        onClick={() => setIsDialogOpen(true)}
+      >
+        <AddIcon />
+      </Fab>
 
-			{ /*Filters*/ }
-			<Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)} aria-labelledby="filters-dialog-title">
-				<DialogTitle id="filters-dialog-title">Filters</DialogTitle>
-				<DialogContent>
-					<DialogContentText>
-						Ratings
-					</DialogContentText>
-					<Rating name="sitter-ratings" defaultValue={0} onChange={handleInputChange} values={values.rating} size="large"/>
-					<DialogContentText>
-						($) Price
-					</DialogContentText>
-					<Slider min={0} max={300} name="price-range" value={values.price} onChange={handleSliders("price")}  valueLabelDisplay="auto" aria-labelledby="price-slider" getAriaValueText={valuePriceText}/>
-					<DialogContentText>
-						Availability
-					</DialogContentText>
-					<MuiPickersUtilsProvider utils={DateFnsUtils}>
-						<DateTimePicker label="From" value={values.date} disablePast onChange={handleDateChange} showTodayButton/>
-						<DateTimePicker label="To" value={values.date} disablePast onChange={handleDateChange} showTodayButton/>
-					</MuiPickersUtilsProvider>
-					<DialogContentText>
-						Sort By
-					</DialogContentText>
-				</DialogContent>
-			</Dialog>
-		</Grid>
-	);
-}
+      {/*Filters*/}
+      <Dialog
+        open={isDialogOpen}
+        onClose={() => setIsDialogOpen(false)}
+        aria-labelledby="filters-dialog-title"
+      >
+        <DialogTitle id="filters-dialog-title" className={classes.title}>Filter Options</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Ratings</DialogContentText>
+          <Rating
+            name="sitter-ratings"
+            defaultValue={0}
+            onChange={handleInputChange}
+            values={values.rating}
+            size="large"
+          />
+          <DialogContentText>($) Price</DialogContentText>
+          <Slider
+            min={0}
+            max={300}
+            name="price-range"
+            value={values.price}
+            onChange={handleSliders("price")}
+            valueLabelDisplay="auto"
+            aria-labelledby="price-slider"
+            getAriaValueText={valuePriceText}
+          />
+          <DialogContentText>Availability</DialogContentText>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <DateTimePicker
+              label="From"
+              value={values.date}
+              disablePast
+              onChange={handleDateChange("fromDate")}
+              showTodayButton
+              className={classes.fromDate}
+              value={values.fromDate}
+            />
+            <DateTimePicker
+              label="To"
+              value={values.date}
+              disablePast
+              onChange={handleDateChange("toDate")}
+              minDate={addDays(values.fromDate, 1)}
+              value={values.toDate}
+              disabled={values.fromDate === null}
+            />
+          </MuiPickersUtilsProvider>
+          <DialogContentText>Sort By</DialogContentText>
+          <FormControl>
+            <RadioGroup
+              aria-label="sorting category"
+              name="sortBy"
+              value={values.sortBy}
+              onChange={handleInputChange}
+            >
+              <FormControlLabel
+                value="sitters"
+                control={<Radio color="primary" />}
+                label="Dog Sitters"
+              />
+              <FormControlLabel
+                value="location"
+                control={<Radio color="primary" />}
+                label="Location"
+              />
+            </RadioGroup>
+          </FormControl>
+        </DialogContent>
+      </Dialog>
+    </Grid>
+  );
+};
 
 export default SearchFilter;

b353a25df1ede9228561a14fe58b2e7347fc4d64
Dan Proctor <dproc96@gmail.com>
2020-10-28 21:36:10 -0400

    Update socket.js

--- client/src/contexts/socket.js
+++ client/src/contexts/socket.js
@@ -1,16 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef } from 'react';
 import socketClient from "socket.io-client";
 import { useProfileContext } from './profile';
 
 function SocketHandler({children}) {
     const socket = socketClient(window.location.origin + "/")
     const {profile, setProfile} = useProfileContext()
-    if (profile) {
+    const oldProfile = useRef(profile)
+    if (profile && (!oldProfile.current || oldProfile.current._id !== profile._id)) {
         socket.emit("profile", profile._id);
         socket.on("notification", notification => {
             profile.notifications.push(notification)
             setProfile(profile)
         })
+        oldProfile.current = profile
     }
     return (
         <Fragment>

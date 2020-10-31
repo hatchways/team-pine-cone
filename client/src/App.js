e56aa31009492c43f9cc072f1e1ad63e0febeb92
Dan Proctor <dproc96@gmail.com>
2020-10-31 10:22:14 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,46 +11,35 @@ import ProfileSettings from "./pages/ProfileSettings";
 import ProfileDetails from "./pages/ProfileDetails";
 import ProfileListings from "./pages/ProfileListings";
 import BecomeASitter from "./pages/BecomeASitter";
-import PaymentSuccess from "./pages/PaymentSuccess";
 import { AuthProvider } from "./contexts/user";
 import PrivateRoute from "./components/PrivateRoute/";
 import ProfileProvider from "./contexts/profile";
 import MySitters from "./pages/MySitters";
 import MyJobs from "./pages/MyJobs";
-import { STRIPE_PUBLIC_KEY } from "./data/stripe";
-
-const stripe = loadStripe(STRIPE_PUBLIC_KEY);
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Elements stripe={stripe}>
-          <AuthProvider>
-            <Route exact path="/signup" component={SignUp} />
-            <Route path="/login" component={Login} />
-            <ProfileProvider>
-              <Navbar />
-              <Route exact path="/" />
-              <PrivateRoute path="/me" component={ProfileSettings} />
-              <Route exact path="/profiles/" component={ProfileListings} />
-              <Route path="/profiles/:id" component={ProfileDetails} />
-              <PrivateRoute
-                exact
-                path="/payment/success"
-                component={PaymentSuccess}
-              />
-              <PrivateRoute
-                exact
-                path="/become-a-sitter"
-                component={BecomeASitter}
-              />
-              <PrivateRoute exact path="/my-sitters" component={MySitters} />
-              <PrivateRoute exact path="/my-jobs" component={MyJobs} />
-              <PrivateRoute exact path="/messages" />
-            </ProfileProvider>
-          </AuthProvider>
-        </Elements>
+        <AuthProvider>
+          <Route exact path="/signup" component={SignUp} />
+          <Route path="/login" component={Login} />
+          <ProfileProvider>
+            <Navbar />
+            <Route exact path="/" />
+            <PrivateRoute path="/me" component={ProfileSettings} />
+            <Route exact path="/profiles/" component={ProfileListings} />
+            <Route path="/profiles/:id" component={ProfileDetails} />
+            <PrivateRoute
+              exact
+              path="/become-a-sitter"
+              component={BecomeASitter}
+            />
+            <PrivateRoute exact path="/my-sitters" component={MySitters} />
+            <PrivateRoute exact path="/my-jobs" component={MyJobs} />
+            <PrivateRoute exact path="/messages" />
+          </ProfileProvider>
+        </AuthProvider>
       </BrowserRouter>
     </MuiThemeProvider>
   );

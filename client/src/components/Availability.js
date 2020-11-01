657ea9ec7c2f8c4e929da20cfb8361bb4e4e9e75
Dan Proctor <dproc96@gmail.com>
2020-11-01 14:51:12 -0500

    Update Availability.js
    Cleaned up availability

--- client/src/components/Availability.js
+++ client/src/components/Availability.js
@@ -1,76 +1,8 @@
-import { Grid, makeStyles } from "@material-ui/core";
-import React, { useState } from "react";
-import { useProfileContext } from "../contexts/profile";
+import { Grid } from "@material-ui/core";
+import React from "react";
 import Calendar from "./Calendar";
 
-const useStyles = makeStyles(theme => ({
-  row: {
-    display: "flex",
-    flexWrap: "wrap",
-    alignItems: "center",
-    width: "100%",
-    marginBottom: 10,
-    color: theme.palette.primary.light,
-    padding: 10,
-    [theme.breakpoints.between("xs", "sm")]: {
-      flexDirection: "column"
-    }
-  },
-  label: {
-    margin: "0px 25px",
-    fontWeight: 700,
-    [theme.breakpoints.between("xs", "sm")]: {
-      margin: 25
-    }
-  },
-  date: {
-    padding: 5
-  }
-}));
-
 function Availability() {
-  const classes = useStyles();
-  const { profile, setProfile } = useProfileContext();
-  const [availability, setAvailability] = useState((profile && profile.availability) || []);
-  const createChangeHandler = (i, key) => {
-    return e => {
-      const newAvail = [...availability];
-      newAvail[i][key] = e;
-      if (newAvail[i].end < newAvail[i].start) {
-        newAvail[i].end = newAvail[i].start;
-      }
-      updateAvailability(newAvail);
-    };
-  };
-  const updateAvailability = newAvail => {
-    const newProfile = { ...profile };
-    newProfile.availability = newAvail;
-    setAvailability(newAvail);
-    setProfile(newProfile);
-    const options = {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newProfile),
-    };
-    fetch(`/profile/${profile._id}`, options);
-  };
-  const addRange = () => {
-    const newAvail = [...availability];
-    newAvail.push({
-      start: new Date(),
-      end: new Date()
-    });
-    updateAvailability(newAvail);
-  };
-  const createDeleteHandler = i => {
-    return () => {
-      const newAvail = [...availability];
-      newAvail.splice(i, 1);
-      updateAvailability(newAvail);
-    };
-  };
   return (
     <Grid container direction="column" alignItems="center">
       <Grid item>

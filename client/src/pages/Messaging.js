3c265a232d0fd17efec6da7b1f1f77e3fe78980a
Dan Proctor <dproc96@gmail.com>
2020-11-04 17:08:49 -0500

    Update Messaging.js

--- client/src/pages/Messaging.js
+++ client/src/pages/Messaging.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useProfileContext } from '../contexts/profile';
 
 function Messaging(props) {
+    const {profile} = useProfileContext();
+    console.log(profile.conversations);
     return (
         <div>
             

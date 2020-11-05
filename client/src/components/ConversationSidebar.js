441633ae2e327c589e562314ff4509c6a0a380af
Dan Proctor <dproc96@gmail.com>
2020-11-04 21:26:35 -0500

    Update ConversationSidebar.js

--- client/src/components/ConversationSidebar.js
+++ client/src/components/ConversationSidebar.js
@@ -1,7 +1,7 @@
-import { Avatar, Grid, makeStyles } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useProfileContext } from '../contexts/profile';
+import { Avatar, Badge, Grid, makeStyles } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useProfileContext } from "../contexts/profile";
 
 const useStyles = makeStyles({
   linkText: {
@@ -9,51 +9,58 @@ const useStyles = makeStyles({
     color: "#222222",
   },
   avatar: {
-    margin: 10
+    margin: 10,
   },
   conversation: {
-    borderBottom: "1px solid #cccccc"
+    borderBottom: "1px solid #cccccc",
   },
   message: {
     marginLeft: 30,
-    fontSize: 12
+    fontSize: 12,
+  },
+  badge: {
+    marginLeft: 20
   }
 });
 
-function ConversationSidebar({user_id, sitter_id, _id, messages}) {
-    const { profile, getProfile } = useProfileContext();
-    const [name, setName] = useState("");
-    const [src, setSrc] = useState(null);
-    const classes = useStyles();
-    useEffect(() => {
-        const partnerId = profile._id === user_id ? sitter_id : user_id;
-        getProfile(partnerId).then(partner => {
-            setName(`${partner.firstName} ${partner.lastName}`);
-            setSrc(partner.photo);
-        })
-    }, [setName, setSrc, profile, user_id, sitter_id, getProfile])
-    return (
-      <Link className={classes.linkText} to={`/messages/${_id}`}>
-        <Grid container direction="column" className={classes.conversation}>
-          <Grid item>
-            <Grid container alignItems="center">
-              <Avatar className={classes.avatar} src={src} />
-              <h3>{name}</h3>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <p className={classes.message}>
-              {messages[0] &&
-                `${
-                  messages[messages.length - 1].sender === profile._id
-                    ? "You"
-                    : name
-                }: ${messages[messages.length - 1].body}`}
-            </p>
+function ConversationSidebar({ user_id, sitter_id, _id, messages }) {
+  const { profile, getProfile } = useProfileContext();
+  const [name, setName] = useState("");
+  const [src, setSrc] = useState(null);
+  const classes = useStyles();
+  useEffect(() => {
+    const partnerId = profile._id === user_id ? sitter_id : user_id;
+    getProfile(partnerId).then((partner) => {
+      setName(`${partner.firstName} ${partner.lastName}`);
+      setSrc(partner.photo);
+    });
+  }, [setName, setSrc, profile, user_id, sitter_id, getProfile]);
+  const read =
+    profile && profile._id === user_id ? "read_by_user" : "read_by_sitter";
+  const newMessages = messages.filter((x) => !x[read]).length;
+  return (
+    <Link className={classes.linkText} to={`/messages/${_id}`}>
+      <Grid container direction="column" className={classes.conversation}>
+        <Grid item>
+          <Grid container alignItems="center">
+            <Avatar className={classes.avatar} src={src} />
+            <h3>{name}</h3>
+            <Badge className={classes.badge} color="primary" badgeContent={newMessages} />
           </Grid>
         </Grid>
-      </Link>
-    );
+        <Grid item>
+          <p className={classes.message}>
+            {messages[0] &&
+              `${
+                messages[messages.length - 1].sender === profile._id
+                  ? "You"
+                  : name
+              }: ${messages[messages.length - 1].body}`}
+          </p>
+        </Grid>
+      </Grid>
+    </Link>
+  );
 }
 
 export default ConversationSidebar;

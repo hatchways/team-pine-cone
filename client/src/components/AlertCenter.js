2f329fc364091cf4b56c3ece3f1f5b4dc1c41095
Michael Braga <michael_braga@outlook.com>
2020-11-05 14:09:03 -0500

    Completed

--- client/src/components/AlertCenter.js
+++ client/src/components/AlertCenter.js
@@ -1,25 +1,52 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { useProfileContext } from "../contexts/profile";
+import React, { useState, useRef, useEffect } from "react";
+import { socket } from "../services/socket";
+import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 
-const AlertCenter = function() {
-	const { profile } = useProfileContext();
-	const player = useRef(document.getElementById("player"));
-	const [count, setCount] = useState(0);
+const AlertCenter = function () {
+  const player = useRef(document.getElementById("player"));
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
 
-	useEffect(() => {
-		if (profile) { 
-			setCount(profile.notifications.length);
-			if (profile.notifications.length > count) { 
-				player.current.play();
-			}
-		}
-	}, [profile]);
-	console.log(profile && profile.notifications)
+  const handleClose = (_, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
 
-	return (
-		<div>
-		</div>
-	);
-}
+  useEffect(() => {
+    const handler = (profile) => {
+      if (profile.notifications.length > 0) {
+        const last = profile.notifications[profile.notifications.length - 1];
+        if (last.title === "New Request") {
+          player.current.play();
+          setMessage(
+            `A new Request from ${profile.firstName} ${profile.lastName}`
+          );
+          setOpen(true);
+        }
+      }
+    };
+    socket.on("update", handler);
+
+    return () => socket.off("update", handler);
+  }, []);
+
+  return (
+    <div>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert
+          variant="filled"
+          elevation={6}
+          onClose={handleClose}
+          severity="success"
+        >
+          {message}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+};
 
 export default AlertCenter;

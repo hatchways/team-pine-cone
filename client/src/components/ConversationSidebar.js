55f4ce9b1354fd1f6d337ace87ac09290715e0a6
Dan Proctor <dproc96@gmail.com>
2020-11-04 19:17:46 -0500

    Update ConversationSidebar.js

--- client/src/components/ConversationSidebar.js
+++ client/src/components/ConversationSidebar.js
@@ -1,23 +1,47 @@
-import { Avatar } from '@material-ui/core';
+import { Avatar, Grid, makeStyles } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useProfileContext } from '../contexts/profile';
 
-function ConversationSidebar({user_id, sitter_id, messages}) {
+const useStyles = makeStyles({
+  linkText: {
+    textDecoration: "none",
+    color: "#222222",
+  },
+  avatar: {
+    margin: 10
+  },
+  conversation: {
+    borderBottom: "1px solid #cccccc"
+  }
+});
+
+function ConversationSidebar({user_id, sitter_id, _id, messages}) {
     const { profile, getProfile } = useProfileContext();
     const [name, setName] = useState("");
     const [src, setSrc] = useState(null);
+    const classes = useStyles();
     useEffect(() => {
         const partnerId = profile._id === user_id ? sitter_id : user_id;
         getProfile(partnerId).then(partner => {
             setName(`${partner.firstName} ${partner.lastName}`);
             setSrc(partner.photo);
         })
-    }, [setName, setSrc])
+    }, [setName, setSrc, profile, user_id, sitter_id, getProfile])
     return (
-        <div>
-            <Avatar src={src} />
-            <h3>{name}</h3>
-        </div>
+      <Link className={classes.linkText} to={`/messages/${_id}`}>
+          <Grid container direction="column" className={classes.conversation} >
+              <Grid item>
+                <Grid container alignItems="center">
+                    <Avatar className={classes.avatar} src={src} />
+                    <h3>{name}</h3>
+                </Grid>
+              </Grid>
+              <Grid item>
+                  <p>{messages[0]}</p>
+              </Grid>
+          </Grid>
+      </Link>
     );
 }
 

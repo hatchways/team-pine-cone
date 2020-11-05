be89e5ee9e05f212e37437cff79cae314db34e2b
Dan Proctor <dproc96@gmail.com>
2020-11-04 21:59:13 -0500

    Update socket.js

--- server/utils/socket.js
+++ server/utils/socket.js
@@ -1,3 +1,4 @@
+const { Message } = require("../models");
 const Conversation = require("../models/Conversation");
 const Profile = require("../models/Profile");
 const notifier = require("./notification");
@@ -21,44 +22,72 @@ exports.create = server => {
 
     socket.on("message", ({conversationId, message}) => {
       Conversation.findById(conversationId).then(conversation => {
-        conversation.messages.push(message);
-        conversation.save().then(() => {
-          Profile.findById(conversation.sitter_id)
-            .populate("requests")
-            .populate("conversations")
-            .then((profile) => {
-              io.to(conversation.sitter_id).emit("update", profile);
-            });
-          Profile.findById(conversation.user_id)
-            .populate("requests")
-            .populate("conversations")
-            .then((profile) => {
-              io.to(conversation.user_id).emit("update", profile);
-            });
+        const newMessage = new Message(message);
+        newMessage.save().then(result => {
+          conversation.messages.push(result._id);
+          conversation.save().then(() => {
+            Profile.findById(conversation.sitter_id)
+              .populate("requests")
+              .populate({
+                path: "conversations",
+                populate: {
+                  path: "messages",
+                  model: "Message"
+                }
+              })
+              .then((profile) => {
+                io.to(conversation.sitter_id).emit("update", profile);
+              });
+            Profile.findById(conversation.user_id)
+              .populate("requests")
+              .populate({
+                path: "conversations",
+                populate: {
+                  path: "messages",
+                  model: "Message",
+                },
+              })
+              .then((profile) => {
+                io.to(conversation.user_id).emit("update", profile);
+              });
+          });
         });
       });
     });
 
     socket.on("read messages", ({conversationId, profileId}) => {
-      Conversation.findById(conversationId).then(conversation => {
+      Conversation.findById(conversationId).populate("messages").then(conversation => {
         const read = profileId.toString() === conversation.user_id.toString() ? "read_by_user" : "read_by_sitter";
         conversation.messages.forEach(message => {
           message[read] = true;
+          message.save();
         });
         conversation.save().then(() => {
           Profile.findById(conversation.sitter_id)
             .populate("requests")
-            .populate("conversations")
+            .populate({
+              path: "conversations",
+              populate: {
+                path: "messages",
+                model: "Message",
+              },
+            })
             .then((profile) => {
               io.to(conversation.sitter_id).emit("update", profile);
             });
           Profile.findById(conversation.user_id)
             .populate("requests")
-            .populate("conversations")
+            .populate({
+              path: "conversations",
+              populate: {
+                path: "messages",
+                model: "Message",
+              },
+            })
             .then((profile) => {
               io.to(conversation.user_id).emit("update", profile);
             });
-        })
+        });
       });
     });
   });

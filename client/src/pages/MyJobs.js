21a229fcb501ab1148e7220e5ab5d3cf5a79de3d
Dan Proctor <dproc96@gmail.com>
2020-11-05 22:11:23 -0500

    Update MyJobs.js

--- client/src/pages/MyJobs.js
+++ client/src/pages/MyJobs.js
@@ -3,21 +3,22 @@ import Bookings from '../components/Bookings';
 import { useProfileContext } from '../contexts/profile';
 
 function MyJobs(props) {
-    const {profile, setProfile} = useProfileContext()
+    const {profile, pullProfile} = useProfileContext()
     const readNotifications = useRef(false);
     useEffect(() => {
       if (profile && !readNotifications.current) {
         profile.notifications = profile.notifications.filter(
-          (notification) => notification.link !== "my-jobs"
+          (notification) => notification.link !== "/my-jobs"
         );
-        setProfile(profile);
         fetch(`/profile/${profile._id}`, {
           method: "PUT",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(profile),
-        });
+        }).then(() => {
+          pullProfile();
+        })
         readNotifications.current = true;
       }
     });
